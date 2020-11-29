14a5cdf2b760d4ddf44998076b8de87f92b3eb58
Dylan Sapienza <dylsapienza12@gmail.com>
2020-11-29 12:41:48 -0500

    Base Template and Blocks added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'TobyLerone',
+        'title': 'Blog Post 1',
+        'content': 'First Post!',
+        'date_posted': 'November 29, 2019'
+    },
+
+    {
+        'author': 'Toni Rigatoni',
+        'title': 'Blog Post 2',
+        'content': 'Second Post!',
+        'date_posted': 'November 29, 2019'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
